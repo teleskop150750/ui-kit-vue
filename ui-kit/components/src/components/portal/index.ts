fb866dc4905976ed45764198a1dd0c4a65295043
teleskop150750 <teleskop150750@gmail.com>
2023-04-13 13:31:25 +0300

    feat(components): [NPortal] implements

---
+++ ui-kit/components/src/components/portal/index.ts
@@ -0,0 +1,8 @@
+import { withInstall } from '@nado/ui-kit-utils'
+
+import Portal from './src/NPortal.vue'
+
+export const NPortal = withInstall(Portal)
+export default NPortal
+
+export * from './src/portal.model'
